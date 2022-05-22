1aa7e364c83e784d8a59a4d9c093d2496502944d
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-05-22 15:11:59 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (79.12%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,14 +1,26 @@
 class Solution {
-public:
-    int hammingWeight(uint32_t n) {
+    int solver1(uint32_t n){
         int ans = 0;
         while (n){
-            // cout << n << endl;
             if (n%2)
                 ans++;
             n /= 2;
             
         }
         return ans;
+    }
+    int solver2(uint32_t n){
+        int ans = 0;
+        uint32_t mask = 1;
+        for (int i=0;i<32;++i){
+            if (n&mask)
+                ans++;
+            mask <<= 1;
+        }
+        return ans;
+    }
+public:
+    int hammingWeight(uint32_t n) {
+        return solver2(n);
     }
 };
