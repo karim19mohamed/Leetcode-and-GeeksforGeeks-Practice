7a184a56a70b75bf6eb73ca437651e932ecc7c75
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-04 23:52:05 +0200

    Time: 27 ms (74.11%), Space: 19.2 MB (9.98%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -12,32 +12,36 @@ class Solution {
         }
         return ratings;
     }
+    void move_forward(vector<int> &candy, vector<int> new_ratings){
+        int sz = new_ratings.size(),
+            num = 1;
+        candy[0] = 1;
+        for (int i=1;i<sz;++i){
+            if (new_ratings[i]>new_ratings[i-1])
+                ++num;
+            else
+                num = 1;
+            candy[i] = num;
+        }
+    }
+    void move_backward(vector<int> &candy, vector<int> new_ratings){
+        int sz = new_ratings.size();
+        for (int i=sz-2;i>-1;--i)
+            if (new_ratings[i]>new_ratings[i+1])
+                if (candy[i]<=candy[i+1])
+                    candy[i] = candy[i+1] + 1;
+    }
 public:
     int candy(vector<int>& ratings) {
         vector<int> new_ratings = modify_zeros(ratings);
         int ans = 0,
-            sz = new_ratings.size(),
-            num = 1;
+            sz = new_ratings.size();
         vector<int> candy(sz,0);
-        candy[0] = 1;
-        for (int i=1;i<sz;++i){
-            if (new_ratings[i]>new_ratings[i-1]){
-                ++num;
-            }else{
-                num = 1;
-            }
-            candy[i] = num;
-        }
-        for (int i=sz-2;i>-1;--i){
-            if (new_ratings[i]>new_ratings[i+1]){
-                if (candy[i]<=candy[i+1]){
-                    candy[i] = candy[i+1] + 1;
-                }
-            }
-        }
+        move_forward(candy, new_ratings);
+        move_backward(candy, new_ratings);
         for (auto c : candy)
             ans += c;
         return ans;
     }
 };
-// 1+2+1+2+3+4+5+1+1+1+2
+
