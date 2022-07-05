9cf1916ce55038912fabdd52398f55cdea83473e
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-05 14:52:12 +0200

    Time: 134 ms (21.93%), Space: 32.4 MB (39.22%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,4 +1,13 @@
 class Solution {
+    void greedy(int& a1, int& a2, vector<int>& v){
+        int sz = v.size();
+        for (int i=0;i<sz-1;++i){
+            if (v[i+1] - v[i] > a2 - a1){
+                a1 = v[i];
+                a2 = v[i+1];
+            }
+        }
+    }
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         const int M = 1e9 + 7;
@@ -8,20 +17,10 @@ public:
         verticalCuts.push_back(w);
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
-        int x1 = 0, x2 = 0, szh = horizontalCuts.size();
-        for (int i=0;i<szh-1;++i){
-            if (horizontalCuts[i+1] - horizontalCuts[i] > x2 - x1){
-                x1 = horizontalCuts[i];
-                x2 = horizontalCuts[i+1];
-            }
-        }
-        int y1 = 0, y2 = 0, szv = verticalCuts.size();
-        for (int i=0;i<szv-1;++i){
-            if (verticalCuts[i+1] - verticalCuts[i] > y2 - y1){
-                y1 = verticalCuts[i];
-                y2 = verticalCuts[i+1];
-            }
-        }
+        int x1 = 0, x2 = 0;
+        greedy(x1, x2, horizontalCuts);
+        int y1 = 0, y2 = 0;
+        greedy(y1, y2, verticalCuts);
         long long ans = ( (long long)((x2-x1)%M) * (long long)((y2-y1)%M) ) % M;
         return ans;
     }
