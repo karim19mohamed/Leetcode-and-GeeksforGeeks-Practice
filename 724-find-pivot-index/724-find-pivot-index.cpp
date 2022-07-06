7d08576a7de9def61cb1100f97e8f70f80c2727f
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-06 12:01:05 +0200

    Time: 65 ms (8.59%), Space: 31.7 MB (17.95%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,20 +3,17 @@ class Solution {
         int sz = nums.size();
         vector<int> res(sz,0);
         res[0] = nums[0];
-        for(int i=1;i<sz;++i){
+        for(int i=1;i<sz;++i)
             res[i] = res[i-1] + nums[i];
-        }
         return res;
     }
 public:
     int pivotIndex(vector<int>& nums) {
         int sz = nums.size();
         vector<int> prefix_sum = runningSum(nums);
-        for (int i=0;i<sz;++i){
-            if (prefix_sum[i]-nums[i] == prefix_sum[sz-1]-prefix_sum[i]){
+        for (int i=0;i<sz;++i)
+            if (prefix_sum[i]-nums[i] == prefix_sum[sz-1]-prefix_sum[i])
                 return i;
-            }
-        }
         return -1;
     }
 };
