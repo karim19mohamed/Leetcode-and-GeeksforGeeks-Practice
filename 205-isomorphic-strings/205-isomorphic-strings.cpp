0420cc560c02bc3843de4061f7b1435e0f0aba7b
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-08 14:14:43 +0200

    Time: 4 ms (91.00%), Space: 7 MB (75.31%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int replacement[130];
+        bool taken[130];
+        memset(replacement,0,sizeof(replacement));
+        memset(taken,0,sizeof(taken));
+        int sz = s.size();
+        for (int i=0;i<sz;++i){
+            int cur = s[i];
+            if (replacement[cur] == 0){
+                if (s[i]!=t[i]){
+                    if (!taken[t[i]]){
+                        replacement[cur] = t[i];
+                        taken[t[i]] = 1;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    if (!taken[t[i]]){
+                        replacement[cur] = t[i];
+                        taken[t[i]] = 1;
+                    }else{
+                        return false;
+                    }
+                }
+            }else{
+                char rep = replacement[cur];
+                if (rep != t[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
