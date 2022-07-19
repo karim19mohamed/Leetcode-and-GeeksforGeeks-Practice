39c5e555892e25b86a00c29dd21c65ee2be105f3
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-19 04:49:48 +0200

    Time: 3 ms (52.03%), Space: 6.3 MB (72.15%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int space1 = 0, space2 = 0;
+        int pt1 = s.size()-1, pt2 = t.size()-1;
+        while(pt1>-1 || pt2>-1){
+            bool flag = 0;
+            while (pt1>-1 && s[pt1]=='#'){
+                ++space1;
+                --pt1;
+                flag = 1;
+            }
+            while (pt2>-1 && t[pt2]=='#'){
+                ++space2;
+                --pt2;
+                flag = 1;
+            }
+            // if (flag)
+            //     continue;
+            while(pt1>-1 && space1 && s[pt1]!='#'){
+                --pt1;
+                --space1;
+                flag = 1;
+            }
+            while(pt2>-1 && space2 && t[pt2]!='#'){
+                --pt2;
+                --space2;
+                flag = 1;
+            }
+            if (flag)
+                continue;
+            if (pt1>-1 && pt2>-1 && s[pt1]!='#' && t[pt2]!='#'){
+                if (s[pt1]==t[pt2]){
+                    --pt1;
+                    --pt2;
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+};
