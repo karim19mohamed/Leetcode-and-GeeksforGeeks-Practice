b07c062300e547ecf59f23b65a20a94c6733aa3a
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-24 09:44:49 +0200

    Time: 354 ms (16.09%), Space: 14.8 MB (68.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int m = 0;
+    int n = 0;
+    bool binary_search(vector<int> &m, int &target){
+        int st = 0, en = n-1;
+        while(st<en){ // FFFFFFFTTTT
+            int mid = st + (en-st)/2;
+            (m[mid]>=target)? en = mid : st = mid + 1;
+        }
+        return m[st]==target;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        m = matrix.size(), n = matrix[0].size();
+        bool ans = false;
+        for (int i=0;i<m;++i){
+            if (target>=matrix[i][0] && target<=matrix[i][n-1])
+                ans |= binary_search(matrix[i], target);
+        }
+        return ans;
+    }
+};
