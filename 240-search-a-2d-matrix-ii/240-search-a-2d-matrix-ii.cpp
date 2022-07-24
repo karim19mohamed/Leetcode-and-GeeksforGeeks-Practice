9496e9de24a3f1f75010746282441c8c932ebcb8
Karim Ibrahim <s.karim.ibrahim@gmail.com>
2022-07-24 09:57:02 +0200

    Time: 411 ms (12.69%), Space: 14.9 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -14,6 +14,8 @@ public:
         m = matrix.size(), n = matrix[0].size();
         bool ans = false;
         for (int i=0;i<m;++i){
+            if (target<matrix[i][0])
+                break;
             if (target>=matrix[i][0] && target<=matrix[i][n-1])
                 ans |= binary_search(matrix[i], target);
         }
